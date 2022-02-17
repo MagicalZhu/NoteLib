596b2f8dde62bc97201b69a20cff562065abddad
huakucha <zhuyuliangm@outlook.com>
2022-02-17 20:21:52 +0800

    fix bug

--- src/components/Highlight/index.js
+++ src/components/Highlight/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 export const Highlight = (props) => (
   <span
@@ -22,30 +21,3 @@ export default Highlight;
 //   <MDXProvider components={components}>
 //     <main {...props} />
 //   </MDXProvider>
-=======
-
-import React from "react";
-export const Highlight = (props) => (
-  <span
-    style={{ color: props.color }}>
-    {props.children}
-  </span>
-);
-export default Highlight;
-
-
-// import React from 'react'
-// import { MDXProvider } from '@mdx-js/react'
-// const components = {
-//   wrapper: props => (
-//     <div style={{ padding: '20px', backgroundColor: 'tomato' }}>
-//       <main {...props} />
-//     </div>
-//   )
-// }
-// export default props => (
-//   <MDXProvider components={components}>
-//     <main {...props} />
-//   </MDXProvider>
->>>>>>> edeb359f18e97e9f877a6280b3bf131a134f2643
-// )
