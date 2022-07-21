860b4e4e7b2b3d9f4d49dde64711cb766cc43b43
huakucha <zhuyuliangm@outlook.com>
2022-07-21 15:43:26 +0800

    Update footer.js

--- footer.js
+++ footer.js
@@ -8,24 +8,8 @@ const footer = {
           href: 'https://v3.cn.vuejs.org/'
         },
         {
-          label: 'Element-Plus',
-          href: 'https://element-plus.gitee.io/zh-CN/'
-        },
-        {
           label: 'React',
           href: 'https://react.docschina.org/'
-        },
-        {
-          label: 'Ant Design',
-          href: 'https://ant.design/index-cn'
-        },
-        {
-          label: 'Electron',
-          href: 'https://www.electronjs.org/'
-        },
-        {
-          label: 'Node.js',
-          href: 'http://nodejs.cn/api/'
         },
         {
           label: 'TypeScript',
@@ -38,46 +22,20 @@ const footer = {
       ],
     },
     {
-      title: '🔗前端开源工具库',
-      items: [
-        { label: 'Lodash', href: 'https://www.lodashjs.com/'},
-        { label: 'Day.js', href: 'https://dayjs.gitee.io/docs/zh-CN/installation/installation'},
-        { label: 'Tailwindcss', href: 'https://www.tailwindcss.cn/docs'},
-        { label: 'Sortablejs', href: 'http://www.sortablejs.com/index.html'},
-        { label: 'Axios', href: 'https://www.axios-http.cn/docs/intro'},
-        { label: 'Dplayer', href: 'http://dplayer.js.org/zh/'},
-        { label: 'Sequelize', href: 'https://www.sequelize.com.cn/core-concepts/getting-started'},
-      ],
-    },
-    {
       title: '🔗后端链接',
       items: [
         { label: 'Spring', href: 'https://spring.io/'},
-        { label: 'Mybatis', href: 'https://mybatis.org/mybatis-3/zh/index.html'},
-        { label: 'Redis', href: 'https://redis.io/documentation'},
         { label: 'Docker', href: 'https://docs.docker.com/get-started/'},
         { label: 'Kubernetes', href: 'https://kubernetes.io/zh/docs/home/'},
-        { label: 'Nginx', href: 'http://nginx.org/en/docs/'},
-        { label: 'Kafka', href: 'https://kafka.apache.org/documentation/#gettingStarted'},
-        { label: 'RabbitMq', href: 'https://www.rabbitmq.com/documentation.html'},
-        { label: 'Pulsar', href: 'https://pulsar.apache.org/docs/zh-CN/standalone/'},
-        { label: 'ElasticSearch', href: 'https://www.elastic.co/guide/en/elasticsearch/reference/current/elasticsearch-intro.html'},
-        { label: 'Dubbo', href: 'https://dubbo.apache.org/zh/docs/quick-start/'},
         { label: 'JVM', href: 'https://docs.oracle.com/javase/8/docs/technotes/tools/windows/index.html'},
       ]
     },
     {
-      title: '🔗后端开源工具库',
+      title: '社区',
       items: [
-        { label: 'Mybatis-plus', href: 'https://baomidou.com/'},
-        { label: 'Hutool', href: 'https://www.hutool.cn/docs/#/'},
-        { label: 'Arthas', href: 'https://arthas.gitee.io/index.html'},
-        { label: 'Xxl Job', href: 'https://www.xuxueli.com/xxl-conf/'},
-        { label: 'Mycat', href: 'http://www.mycat.org.cn/'},
-        { label: 'ShardingSphere', href: 'https://shardingsphere.apache.org/document/current/cn/overview/'},
-        { label: 'Shenyu', href: 'https://shenyu.apache.org/zh/docs/index'},
-        { label: 'Canal', href: 'https://github.com/alibaba/canal/wiki'},
-        { label: 'Seata', href: 'https://seata.io/zh-cn/docs/overview/what-is-seata.html'},
+        { label: '掘金', href: 'https://juejin.cn'},
+        { label: 'InfoQ', href: 'https://www.infoq.cn/'},
+        { label: 'Github', href: 'https://github.com/'},
       ],
     },
     {
@@ -88,16 +46,12 @@ const footer = {
           href: 'https://www.docusaurus.cn/',
         },
         {
-          label: 'Vuepress',
-          href: 'https://vuepress.vuejs.org/zh/',
-        },
-        {
-          label: 'Obsidian',
-          href: 'https://obsidian.md/',
+          label: 'Typora',
+          href: 'https://typoraio.cn',
         },
       ],
     },
   ],
-  copyright: `Copyright © ${new Date().getFullYear()} huakucha. Built By Docusaurus.`
+  copyright: `Copyright © ${new Date().getFullYear()} huakucha. Built With Docusaurus.`
 }
 module.exports = footer
