f5f0ab97cb1c88e502fdc90a5043631fc4a17c96
athu <zhuyuliangm@outlook.com>
2023-05-26 15:02:55 +0800

    init data-link

--- src/data/sidebar/network.js
+++ src/data/sidebar/network.js
@@ -19,12 +19,13 @@ const netWorkSidebar = [
       type: Type.CATEGORY,
       label: "TCP/IP",
       items: [
-          "计算机网络/TCP-IP/net-basic",
-          "计算机网络/TCP-IP/tcp-ip-basic",
+        "计算机网络/TCP-IP/net-basic",
+        "计算机网络/TCP-IP/tcp-ip-basic",
+        "计算机网络/TCP-IP/data-link",
       ],
       collapsed: true,
       link: {
-          type: "generated-index"
+        type: "generated-index"
       }
   }
 ]
